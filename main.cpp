ad7eabbf6b1aa1cf4cdafc0a300e7443b4d300b5
bebidaroja <bebida.roja@gmail.com>
2020-11-23 16:57:55 +0100

    RealAutoTest

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <cstdlib>
 #include <string>
 #include <cstring>
+#include <iostream>
 
 using std::string;
+using std::cout;
+using std::endl;
 
 int main(int argc, char* argv[]){
+	if(argc != 2) {
+		cout << "Usage: update <commitnamewithoutspaces>" << endl;
+		return 0;
+	}
 	system((string("git add --all;git commit -m \"")+string(argv[1],strlen(argv[1]))+string("\"; git push -u origin master")).c_str());
 }
